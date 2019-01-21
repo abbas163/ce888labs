37d65027d02ad46dc348be67ed180f89c849c392
nimitkhanna <nimitkh.msc17@gmail.com>
2019-01-21 14:33:08 +0000

    Completed

--- lab2/vehicles.py
+++ lab2/vehicles.py
@@ -7,14 +7,15 @@ sns.set(style="whitegrid")
 df  = pd.read_csv('./vehicles.csv')
 
 sns_plot = sns.lmplot(df.columns[0], df.columns[1], data=df, fit_reg=False)
+sns_plot.savefig("scaterplot.pdf",bbox_inches='tight')
 plt.show()
 
-from matplotlib import pyplot as plt
-sns_plot  = pd.read_csv('./vehicles.csv')
-
-sb.distplot(sns_plot['Current fleet'],bins=20 ,kde = False)
-
+sb.distplot(sns_plot['Current fleet'],bins=30 ,kde = False)
+sns_plot.savefig("Histogram-Currentfleet.pdf",bbox_inches='tight')
 plt.show()
-sb.distplot(sns_plot['New Fleet'],bins=20 ,kde = False)
 
+
+
+sb.distplot(sns_plot['New Fleet'],bins=30 ,kde = False)
+sns_plot.savefig("Histogram-Newfleet.pdf",bbox_inches='tight')
 plt.show()
