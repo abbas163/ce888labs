db872b1fb3bc1eaa8697ec214ee6a8c0263a680d
nimitkhanna <nimitkh.msc17@gmail.com>
2019-01-27 23:52:53 +0000

    Completed

--- lab2/README.md
+++ lab2/README.md
@@ -1,5 +1,13 @@
-In this Lab-2 First we have created a vehicles.py file in Pycharm, the we have readed the vehicle.csv file to Create and save  scatterplots and  histograms for the current fleet and the proposed fleet (New_Fleet).
+In this Lab-2, First we have created a vehicles.py file in Pycharm, then we readed the vehicle.csv file to Create and save scatterplots and histograms for the current fleet and the proposed fleet (New_Fleet) as shown below:
+
 ![logo](./scaterplot.png?raw=true)
-![logo](./bootstrap_confidence.png?raw=true)
+
 ![logo](./current_fleet_histogram.png?raw=true)
+
 ![logo](./New_Fleet_Histogram.png?raw=true)
+
+
+Then, We have used the bootstrap.py to creatd a function bootstrap for calculating bootstrap confidence i.e Mean, Lower and Upper as shown below
+
+
+![logo](./bootstrap_confidence.png?raw=true)
--- lab2/bootstrap.py
+++ lab2/bootstrap.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 
-def boostrap(sample, sample_size, iterations):
+def bootstrap(sample, sample_size, iterations):
     # <---INSERT YOUR CODE HERE--->
     array = np.empty([iterations, sample_size])
     mean = np.empty([iterations, 1])
@@ -28,12 +28,12 @@ if __name__ == "__main__":
     data = df.values.T[1]
     boots = []
     for i in range(100, 100000, 1000):
-        boot = boostrap(data, data.shape[0], i)
+        boot = bootstrap(data, data.shape[0], i)
         boots.append([i, boot[0], "mean"])
         boots.append([i, boot[1], "lower"])
         boots.append([i, boot[2], "upper"])
 
-    df_boot = pd.DataFrame(boots, columns=['Boostrap Iterations', 'Mean', "Value"])
+    df_boot = pd.DataFrame(boots, columns=['Bootstrap Iterations', 'Mean', "Value"])
     sns_plot = sns.lmplot(df_boot.columns[0], df_boot.columns[1], data=df_boot, fit_reg=False, hue="Value")
 
     sns_plot.axes[0, 0].set_ylim(0, )
